949b5da011c84eb35f63b8a50b2ca9df7cab6834
舩渡健太 <funato@jms-web.co.jp>
2018-07-09 14:12:04 +0900

    chore Remove unnecessary files

--- Common/JMS_COMMON/My Project/AssemblyInfo.vb
+++ Common/JMS_COMMON/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 ' 既定値にすることができます:
 
-<Assembly: AssemblyVersion("0.9.18190.0306")>
-<Assembly: AssemblyFileVersion("0.9.18190.0306")>
+<Assembly: AssemblyVersion("0.9.18190.0506")>
+<Assembly: AssemblyFileVersion("0.9.18190.0506")>
 <Assembly: NeutralResourcesLanguage("")>
