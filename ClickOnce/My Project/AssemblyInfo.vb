0bf0dc8e5c8a3911a10b44d9479996c57b6e18d2
舩渡健太 <funato@jms-web.co.jp>
2018-10-11 18:46:38 +0900

    fix #86

--- ClickOnce/My Project/AssemblyInfo.vb
+++ ClickOnce/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.9.18284.0339")>
-<Assembly: AssemblyFileVersion("0.9.18284.0339")>
+<Assembly: AssemblyVersion("0.9.18284.0945")>
+<Assembly: AssemblyFileVersion("0.9.18284.0945")>
