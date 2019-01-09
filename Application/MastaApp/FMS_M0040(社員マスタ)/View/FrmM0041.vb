c4ca75f7b2d7b0844d304ef90411328052eb53c0
福田明弘 <fukuta@jms-web.co.jp>
2019-01-09 09:21:58 +0900

    社員マスタで社員IDで999999のデータでダブりエラー発生を修正

--- Application/MastaApp/FMS_M0040(社員マスタ)/View/FrmM0041.vb
+++ Application/MastaApp/FMS_M0040(社員マスタ)/View/FrmM0041.vb
@@ -293,7 +293,7 @@ Public Class FrmM0041
                     sbSQL.Append(" ,DEL_SYAIN_ID")
                     sbSQL.Append(" ) VALUES ( ")
                     'ŽÐˆõID
-                    sbSQL.Append(" (SELECT MAX(SYAIN_ID)+1 FROM M004_SYAIN WHERE SYAIN_ID<999999)")
+                    sbSQL.Append(" (SELECT MAX(SYAIN_ID)+1 FROM M004_SYAIN)")
                     'ŽÐˆõNO
                     sbSQL.Append(" ,'" & Me.mtxSYAIN_NO.Text.Trim & "'")
                     'Ž–¼
