7fc3f2ba04bf11a10c4e27521a0faae77a1f8547
舩渡健太 <funato@jms-web.co.jp>
2021-04-14 15:54:50 +0900

    ライブラリdll追加

--- ClickOnce/My Project/AssemblyInfo.vb
+++ ClickOnce/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 既定値にすることができます:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.9.21091.0841")>
-<Assembly: AssemblyFileVersion("0.9.21091.0841")>
+<Assembly: AssemblyVersion("0.9.21104.0632")>
+<Assembly: AssemblyFileVersion("0.9.21104.0632")>
